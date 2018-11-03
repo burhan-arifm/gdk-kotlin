7cc40eb3b13c31ccd4ddb4d19db8a4f9cf0e72b3
Burhan Arif M <burhan.arifm@hotmail.com>
2018-11-03 10:08:44 +0700

    code refactoring

--- app/src/main/java/burhanarifm/footballclubs/RecyclerViewAdapter.kt
+++ app/src/main/java/burhanarifm/footballclubs/RecyclerViewAdapter.kt
@@ -5,9 +5,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.*
 
 class RecyclerViewAdapter(private val context: Context, private val items: List<Item>, private val listener: (Item) -> Unit)
     : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
@@ -21,10 +21,8 @@ class RecyclerViewAdapter(private val context: Context, private val items: List<
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-
-        private val name = view.findViewById<TextView>(R.id.name)
-        private val image = view.findViewById<ImageView>(R.id.image)
+    class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+        LayoutContainer {
 
         fun bindItem(items: Item, listener: (Item) -> Unit) {
             name.text = items.name
